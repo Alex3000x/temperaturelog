43623e2283cd9580cf5f35991175ff16a50c93f9
Alex3000x <79801299+Alex3000x@users.noreply.github.com>
2021-05-12 17:08:37 +0200

    Add files via upload

---
+++ azione_di_eliminazione.php
@@ -0,0 +1,37 @@
+<?php
+
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+  $url = "https://";   
+else
+  $url = "http://";   
+
+$url.= $_SERVER['HTTP_HOST'];   
+$url.= $_SERVER['REQUEST_URI'];   
+
+$url_components = parse_url($url);
+parse_str($url_components['query'], $params);
+
+$idutente = $params["id"];
+
+$connection = mysqli_connect("localhost","root","","temperaturelog") or die("Connessione non riuscita");
+$query = "DELETE
+          FROM utenti
+          WHERE idutente = $idutente";
+          
+mysqli_query($connection,$query);
+mysqli_close($connection);
+print "
+        <html>
+            <head>
+                <title>temperaturelog</title>
+            </head>
+            <body>
+                <center>
+                <h1>GESTIONE UTENTI</h1><br><br>
+                <h3>Utente eliminato dal database</h3>
+                <a href=index.html>Torna alla home</a>
+                <a href=gestione_utenti.html>Torna alla gestione utenti</a>
+                </center>
+            </body>
+        </html>";
+?>
---
+++ visualizza_utenti.php
@@ -0,0 +1,67 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","","temperaturelog") or die("Connessione non eseguita");
+$query = "SELECT *
+          FROM utenti
+          WHERE 1";
+$result1 = mysqli_query($connection, $query);
+
+print "
+        <html>
+            <head>
+                <title>temperaturelog</title>
+            </head>
+            <body>
+                <center>
+                    <h1>GESTIONE UTENTI</h1>
+                    <table border>";
+
+if(mysqli_num_rows($result1) != 0)
+{
+    print "
+                        <tr>
+                            <th>N°</th>
+                            <th>Nome</th>
+                            <th>Cognome</th>
+                            <th>Data di nascita</th>
+                            <th>ID Carta</th>
+                            <th colspan=2>Azioni</th>
+                        </tr>";
+    $i = 1;
+    while ($row = mysqli_fetch_array($result1))
+    {
+        $timestamp = strtotime($row['datanascita']);
+        $date = date('d/m/Y', $timestamp);
+
+        print "
+                        <tr>
+                            <td>$i</td>
+                            <td>$row[nome]</td>
+                            <td>$row[cognome]</td>
+                            <td>$date</td>
+                            <td>$idcarta</td>
+                            <td><a href=modifica_utente.php?id=$row[idutente]><img src=./immagini/modifica.png alt=Modifica></a></td>
+                            <td><a href=azione_di_eliminazione.php?id=$row[idutente]><img src=./immagini/elimina.png alt=Elimina></a></td>
+                        </tr>";
+        $i++;
+    }
+}
+else
+    print "Nessuna persona memorizzata nel database";
+
+print "
+                    </table>
+                </center>
+            </body>";
+mysqli_close($connection);
+print "
+            <footer>
+                <center>
+                    <br>
+                    <form action=inserisci_utente.html>
+                        <input type=submit value=Inserisci&nbsp;utente>
+                    </form>
+                </center>
+            </footer>
+        </html>";
+?>+*
