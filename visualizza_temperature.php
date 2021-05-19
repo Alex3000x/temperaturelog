048e3a779c93a54f5c4fd41a153432ffac74f102
Alex3000x <alessio.gurgoglione@gmail.com>
2021-05-19 18:42:57 +0200

    update sulla visione delle temperature

--- visualizza_temperature.php
+++ visualizza_temperature.php
@@ -1,64 +1,82 @@
+<html>
+    <style>
+        table, th, td {
+            text-align: center;
+            margin-top: 80px;
+            margin-bottom: 20px;
+            border: 1px solid #a8afb7;
+            border-collapse: collapse;
+        }
+    </style>
+    <head>
+        <link rel=icon href=favicon.ico>
+        <link rel = "stylesheet" href="allstyles.css"/>
+        <title>temperaturelog</title>
+    </head>
+    <body>
+        <div class="title">
+            <img class="logo1" src=./immagini/logo1.png alt=T>
+            <h1>emperature</h1>
+            <img class="logo2" src=./immagini/logo2.png alt=T>
+            <h1 class="og">og</h1>
+        </div>
+        <h1>TEMPERATURE</h1>
+        <center>
+            <table class="centro">
+                <tr>
+                    <th>N°</th>
+                    <th>Utente</th>
+                    <th>Temperatura</th>
+                </tr>
 <?php
 
 $connection = mysqli_connect("localhost","root","","temperaturelog") or die("Connessione non eseguita");
 $query = "SELECT *
-          FROM utenti
-          WHERE 1";
+          FROM temperature";
 $result1 = mysqli_query($connection, $query);
-
-print "
-        <html>
-            <head>
-                <link rel=icon href=favicon.ico>
-                <title>temperaturelog</title>
-            </head>
-            <body>
-                <center>
-                    <h1>UTENTI</h1>
-                    <table border>
-                    <p>NON C'È NIENTE!</p>";
-/*
-                    
 if(mysqli_num_rows($result1) != 0)
 {
-    print "
-                        <tr>
-                            <th>N°</th>
-                            <th>Nome</th>
-                            <th>Cognome</th>
-                            <th>Data di nascita</th>
-                            <th colspan=2>Azioni</th>
-                        </tr>";
+    $temperatura = 0;
     $i = 1;
     while ($row = mysqli_fetch_array($result1))
     {
-        $timestamp = strtotime($row['datanascita']);
-        $date = date('d/m/Y', $timestamp);
-
+        $temperatura = $row['temperatura'];
         print "
-                        <tr>
-                            <td>$i</td>
-                            <td>$row[nome]</td>
-                            <td>$row[cognome]</td>
-                            <td>$date</td>
-                            <td><a href=modifica_dati_utente.php?id=$row[idutente]><img src=./immagini/modifica.png alt=Modifica></a></td>
-                            <td><a href=elimina_utente.php?id=$row[idutente]><img src=./immagini/elimina.png alt=Elimina></a></td>
-                        </tr>";
+                <tr>
+                    <td>$i</td>";
+        
+        $connection = mysqli_connect("localhost","root","","temperaturelog") or die("Connessione non eseguita");
+        $query = "SELECT user.idutente, user.nome, user.cognome, temp.idtemperatura, temp.temperatura
+                  FROM utenti AS user, temperature AS temp
+                  WHERE temp.idutente = user.idutente
+                  AND temp.temperatura = '$temperatura'
+                  ORDER BY temp.temperatura";
+        $result2 = mysqli_query($connection, $query);
+        if(mysqli_num_rows($result2) != 0)
+        {
+            while ($row = mysqli_fetch_array($result2))
+            {
+                $nome = $row['nome'];
+                $cognome = $row['cognome'];
+            
+                print "
+                    <td>$row[cognome] $row[nome]</td>";
+            }
+        }
+        print "
+                    <td>$temperatura</td>
+                </tr>";
         $i++;
     }
 }
 else
-    print "Nessun utente memorizzato nel database";*/
+    print "Nessuna smart card memorizzata nel database";
 mysqli_close($connection);
 ?>
 
-                    </table><br>
-                    </form>
-                </center>
-            </body>
-            <footer>
-                <center>
-                    <a href=index.html>Home</a></td>
-                </center>
-            </footer>
-        </html>
+            </table><br>
+            <a class="button" href="index.html">Torna alla home</a></td>
+            </form>
+        </center>
+    </body>
+</html>
